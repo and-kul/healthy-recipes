7442751912d986153ad96413cfca2bc2705a7f31
Ivan <iw.turin2012@yandex.ru>
2019-11-17 04:21:56 +0200

    .

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ def login():
 def check_exists(uuid):
     with sqlite3.connect("log.db") as conn:
         cursor = conn.cursor()
-        cursor.execute('select exists(select 1 from usrs where uuid=?)', uuid)
+        cursor.execute('select exists(select 1 from usrs where uuid=?) as lal', (uuid,))
         conn.commit()
-    return cursor.fetchone() == 1
+    return cursor.fetchone()['lal'] == 1
 
 
 @app.route("/api/nutritional")
@@ -56,32 +56,73 @@ def nutritional():
 
 @app.route("/api/topE")
 def top_5():
-    if request.headers.get('Authorization') is None or check_exists(request.headers.get('Authorization')):
+    if request.headers.get('Authorization') is None:  # or check_exists(request.headers.get('Authorization')):
         abort(403)
     else:
         with sqlite3.connect("log.db") as conn:
             val = pd.read_sql(
-                "select * from data_subset ds join products_v3 p on ds.ean = p.ean where ds.user_id=='old'", conn)
+                "select * from data_subset ds join products_v4 p on ds.ean = p.ean where ds.user_id=='old'", conn)
             e_count = val[[col for col in val.columns if col.startswith('E')]].sum(axis=0)
             return jsonify(e_count.sort_values(ascending=False).iloc[:5].to_dict())
 
 
-@app.route("/api/products")
-def products():
-    if request.headers.get('Authorization') is None:
+@app.route("/api/stats")
+def stats():
+    if request.headers.get('Authorization') is None:  # or check_exists(request.headers.get('Authorization')):
         abort(403)
     else:
         with sqlite3.connect("log.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("""select sum(proteins) as 'proteins',
+            cursor.execute("""select TransactionDate as 'month' ,sum(proteins) as 'proteins',
                                     sum(fats) as 'fats',
                                     sum(carbohydrates) as 'carbohydrates',
-                                    sum(fats_saturated) as 'fats_saturated'
-                                from data_subset ds join products_v3 p on ds.ean = p.ean where ds.user_id=='old'""")
-            row = cursor.fetchone()
-            conn.commit()
-        items = dict(zip([key[0] for key in cursor.description], row))
-        return jsonify(items)
+                                    sum(fats_saturated) as 'fats_saturated',
+                                    sum(sugar) as 'sugar'
+                                from data_subset ds join products_v4 p on ds.ean = p.ean where ds.user_id=='old' group by TransactionDate""")
+            rows = cursor.fetchall()
+            arr = []
+            for row in rows:
+                items = dict(zip([key[0] for key in cursor.description], row))
+                # e count
+                val = pd.read_sql(
+                    "select * from data_subset ds join products_v4 p on ds.ean = p.ean where ds.user_id=='old' and ds.TransactionDate =='" +
+                    row[0] + "'", conn)
+                e_count = val[[col for col in val.columns if col.startswith('E')]].sum(axis=0)
+                res_e = e_count.sort_values(ascending=False).iloc[:5].to_dict()
+
+                arr_e = []
+                for k, v in res_e.items():
+                    cursor = conn.cursor()
+                    cursor.execute(
+                        "select name, description, e, danger_level, side_effects from es where es.e=='" + k + "'")
+                    rows = cursor.fetchone()
+                    vals = dict(zip([key[0] for key in cursor.description], rows))
+                    vals.update({"count": v})
+                    arr_e.append(vals.copy())
+                # bju count
+                items.update({"top": arr_e})
+                # array final
+                arr.append(items.copy())
+        return jsonify(arr)
+
+
+# @app.route("/api/products")
+# def products():
+#     if request.headers.get('Authorization') is None: # or check_exists(request.headers.get('Authorization')):
+#         abort(403)
+#     else:
+#         req_body = request.get_json()
+#         with sqlite3.connect("log.db") as conn:
+#             cursor = conn.cursor()
+#             cursor.execute(
+#                 "select p.pictureUrl as 'picture_url',"
+#                 " marketingName_finnish as 'name_fin',"
+#                 " marketingName_english as 'name_eng' "
+#                 "from data_subset inner join products_v4 p "
+#                 "on data_subset.ean = p.ean where data_subset.TransactionDate = '8'")
+#             d = [dict(zip([key[0] for key in cursor.description], row)) for row in cursor.fetchall()]
+#             conn.commit()
+#         return jsonify(d)
 
 
 if __name__ == '__main__':
